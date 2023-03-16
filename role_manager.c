2e4c2f3466f81fc8c4efdbda5c676e779f21bd9b
吴俊亮 <wujunliang18@mails.ucas.ac.cn>
2023-03-16 12:49:45 +0800

    init

---
+++ role_manager.c
@@ -0,0 +1,462 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define USER2ROLE_PATH "/etc/LiangLSM/user2role"
+#define ROLE2PERMISSION_PATH "/etc/LiangLSM/role2PERMISSION"
+#define CONTROL_PATH "/etc/LiangLSM/control"
+#define MAX_ROLENAME 20
+
+char* permission_list[] = {
+	"create file",
+	"rename file"
+};
+#define PERMISSION_COUNT (sizeof(permission_list)/sizeof(char*))
+
+int write_user2role(int uid, char *role){
+	FILE *fp = fopen(USER2ROLE_PATH, "ab");
+	if(!fp){
+		printf("file open failed\n");
+		return -1;
+	}
+	
+	fwrite(&uid, sizeof(int), 1, fp);
+	fwrite(role, sizeof(char), MAX_ROLENAME+1, fp);
+	
+	fclose(fp);
+	return 1;
+}
+
+int user_exists(int uid){
+	FILE *fp = fopen(USER2ROLE_PATH, "rb");
+	if(!fp){
+		printf("file open failed\n");
+		return -1;
+	}
+	
+	int _uid;
+	while(fread(&_uid, sizeof(int), 1, fp)){
+		if(_uid == uid){
+			fclose(fp);
+			return 1;
+		}
+		fseek(fp, sizeof(char) *(MAX_ROLENAME + 1), SEEK_CUR);
+	}
+	fclose(fp);
+	return 0;
+}
+
+int add_user2role(int uid, char *role){
+	int role_len = strlen(role);
+	if(role_len > MAX_ROLENAME){
+		printf("Role name too long\n");
+		return -1;
+	}
+	if(user_exists(uid)){
+		printf("The role of user already exists\n");
+		return -1;
+	}
+	
+	char _role[MAX_ROLENAME+1];
+	for(int i = 0; i < MAX_ROLENAME; i++){
+		if(i < role_len)
+			_role[i] = role[i];
+		else
+			_role[i] = '\0';
+	}
+	
+	return write_user2role(uid, _role);
+}
+
+int del_user2role(int uid){
+	FILE *fp = fopen(USER2ROLE_PATH, "ab");
+	if(!fp){
+		printf("open file failed\n");
+		return -1;
+	}
+
+	int _uid;
+	char _role[MAX_ROLENAME+1];
+
+	while(fread((char*)&_uid, sizeof(int), 1, fp)){
+		fread(_role, sizeof(char), MAX_ROLENAME+1, fp);
+
+		if(_uid != uid){
+			fseek(fp, -sizeof(int) - sizeof(char) *(MAX_ROLENAME+1), SEEK_CUR);
+			fwrite((char*)&_uid, sizeof(int), 1, fp);
+			fwrite(_role, sizeof(char), MAX_ROLENAME+1, fp);
+		}
+	}
+	
+	fclose(fp);
+	return 1;
+}
+
+int change_user2role(int uid, char *role){
+	if(del_user2role(uid) == 1)
+		return add_user2role(uid, role);
+	else
+		return -1;
+}
+
+int show_user2role(){
+	FILE *fp = fopen(USER2ROLE_PATH, "rb");
+	if(!fp){
+		printf("No items now\n");
+		return -1;
+	}
+
+	int uid;
+	char role[MAX_ROLENAME+1];
+	while(fread((char*)&uid, sizeof(int), 1, fp)){
+		fread(role, sizeof(char), MAX_ROLENAME+1, fp);
+		printf("uid %d : %s\n", uid, role);
+	}
+	
+	fclose(fp);
+	return 1;
+}
+
+
+int write_role(char *role, int *permission){
+	FILE *fp = fopen(ROLE2PERMISSION_PATH, "ab");	
+	if(!fp){
+		printf("file open failed\n");
+		return -1;
+	}
+	
+	fwrite(role, sizeof(char), MAX_ROLENAME+1, fp);
+	fwrite(permission, sizeof(int), PERMISSION_COUNT, fp);
+	fclose(fp);
+	return 1;
+}
+
+int role_exists(char *role){
+	FILE *fp = fopen(ROLE2PERMISSION_PATH, "rb");
+	if(!fp){
+		printf("file open failed\n");
+		return -1;
+	}
+	
+	char _role[MAX_ROLENAME+1];
+	while(fread(_role, sizeof(char), MAX_ROLENAME+1, fp)){
+		if(!strcmp(_role, role)){
+			fclose(fp);
+			return 1;
+		}
+		fseek(fp, sizeof(int) * PERMISSION_COUNT, SEEK_CUR);
+	}
+	
+	fclose(fp);
+	return 0;
+}
+
+int add_role(char *role, int *permission){
+	int role_len = strlen(role);
+	
+	if(role_len > MAX_ROLENAME){
+		printf("Role name too long\n");
+		return -1;
+	}
+	if(sizeof(permission) != PERMISSION_COUNT * sizeof(int)){
+		printf("invalid permission setting\n");
+		return -1;
+	}
+	if(role_exists(role) == 1){
+		printf("Role already exists\n");
+		return -1;
+	}
+			
+	char _role[MAX_ROLENAME+1];
+	for(int i = 0; i < MAX_ROLENAME; i++)
+	{
+		if(i < role_len)
+			_role[i] = role[i];
+		else
+			_role[i] = '\0';
+	}
+	
+	return write_role(_role, permission);
+}
+
+int del_role(char *role){
+	FILE *fp = fopen(ROLE2PERMISSION_PATH, "ab");
+	if(!fp){
+		printf("open file failed\n");
+		return -1;
+	}
+	
+	char _role[MAX_ROLENAME+1];
+	int _permission[PERMISSION_COUNT];
+	while(fread(_role, sizeof(char), MAX_ROLENAME+1, fp))
+	{
+		fread(_permission, sizeof(int), PERMISSION_COUNT, fp);
+
+		if(strcmp(_role, role)){
+			fseek(fp, -sizeof(int) * PERMISSION_COUNT - sizeof(char) *(MAX_ROLENAME+1), SEEK_CUR);
+			fwrite(_role, sizeof(char), MAX_ROLENAME+1, fp);
+			fwrite(_permission, sizeof(int), PERMISSION_COUNT, fp);
+		}
+	}
+
+	fclose(fp);
+	return 1;
+}
+
+int change_role(char *role, int *permission){
+	if(del_role(role) == 1)
+		return add_role(role, permission);
+	else
+		return -1;
+}
+
+int show_roles(){
+	FILE *fp = fopen(ROLE2PERMISSION_PATH, "rb");
+	if(!fp){
+		printf("No items now\n");
+		return -1;
+	}
+
+	int permission[PERMISSION_COUNT];
+	char role[MAX_ROLENAME+1];
+	while(fread(role, sizeof(char), MAX_ROLENAME+1, fp)){
+		printf("%s permission:", role);
+		fread(permission, sizeof(int), PERMISSION_COUNT, fp);
+		for(int i = 0; i < PERMISSION_COUNT; i++){
+			printf(" %s: %s", permission_list[i], permission[1] ? "yes" : "no");
+		}
+	}
+	
+	fclose(fp);
+	return 1;
+}
+
+int get_state(){
+	FILE *fp = fopen(CONTROL_PATH, "rb");
+	if(!fp){
+		printf("file open failed\n");
+		return -1;
+	}
+
+	int state;
+	fread((char*)&state, sizeof(int), 1, fp);
+	
+	return state;
+}
+
+int set_state(int state){
+	FILE *fp = fopen(CONTROL_PATH, "wb");
+	if(!fp){
+		printf("file open failed\n");
+		return 0;
+	}
+
+	fwrite((char*)&state, sizeof(int), 1, fp);
+
+	return 1;
+}
+
+int main(int argc, char *argv[]){
+	if(argc == 1){
+		printf("Error: too few arguments\n");
+		return -1;
+	}
+		
+	if(!strcmp(argv[1], "-s")){
+		if(argc < 3){
+			printf("Error: too few arguments\n");
+			return -1;
+		}
+		if(argc > 3){
+			printf("Error: too many arguments\n");
+			return -1;
+		}
+			
+		if(!strcmp(argv[2], "user2role")){
+			show_user2role();
+			return 0;
+		}
+		else if(!strcmp(argv[2], "roles")){
+			show_roles();
+			return 0;
+		}
+		else{
+			printf("Error: invalid argument\n");
+			return -1;
+		}
+	}
+	else if(!strcmp(argv[1], "-ra")){
+		if(argc < 4){
+			printf("Error: too few arguments\n");
+			return -1;
+		}
+		if(argc > 4){
+			printf("Error: too many arguments\n");
+			return -1;
+		}
+		
+		if(strlen(argv[3]) != PERMISSION_COUNT){
+			printf("Error: invalid permission setting\n");
+			return -1;
+		}
+
+		int permission[PERMISSION_COUNT];
+		for(int i = 0; i < PERMISSION_COUNT; i++){
+			permission[i] = argv[3][i] - '0';
+			if(permission[i] != 0 && permission[i] != 1){
+				printf("Error: invalid permission setting\n");
+				return 0;
+			}
+		}
+		
+		if(add_role(argv[2], permission) == 1)
+			printf("Succeed to add role\n");
+		else
+			printf("Fail to add role\n");
+		
+		return 0;
+	}
+	else if(!strcmp(argv[1], "-ua")){
+		if(argc < 4){
+			printf("Error: too few arguments\n");
+			return -1;
+		}
+		if(argc > 4){
+			printf("Error: too many arguments\n");
+			return -1;
+		}
+		
+		int uid = atoi(argv[2]);
+		if(add_user2role(uid, argv[3]) == 1)
+			printf("Succeed to add the role the of the user\n");
+		else
+			printf("Fail to add the role the of the user\n");
+		
+		return 0;
+	}
+	else if(!strcmp(argv[1], "-rd")){
+		if(argc < 3){
+			printf("Error: too few arguments\n");
+			return -1;
+		}
+		if(argc > 3){
+			printf("Error: too many arguments\n");
+			return -1;
+		}
+		
+		if(del_role(argv[2]) == 1)
+			printf("Succeed to delete role\n");
+		else
+			printf("Fail to adeletedd role\n");
+		
+		return 0;
+	}
+	else if(!strcmp(argv[1], "-ud")){
+		if(argc < 3){
+			printf("Error: too few arguments\n");
+			return -1;
+		}
+		if(argc > 3){
+			printf("Error: too many arguments\n");
+			return -1;
+		}
+		
+		int uid = atoi(argv[2]);
+		if(del_user2role(uid) == 1)
+			printf("Succeed to delete the role the of the user\n");
+		else
+			printf("Fail to delete the role the of the user\n");
+		
+		return 0;
+	}
+	else if(!strcmp(argv[1], "-rc")){
+		if(argc < 4){
+			printf("Error: too few arguments\n");
+			return -1;
+		}
+		if(argc > 4){
+			printf("Error: too many arguments\n");
+			return -1;
+		}
+
+		if(strlen(argv[3]) != PERMISSION_COUNT){
+			printf("Error: invalid permission setting\n");
+			return 0;
+		}
+
+		int permission[PERMISSION_COUNT];
+		for(int i = 0; i < PERMISSION_COUNT; i++){
+			permission[i] = argv[3][i] - '0';
+			if(permission[i] != 0 && permission[i] != 1){
+				printf("Error: invalid permission setting\n");
+				return 0;
+			}
+		}
+		
+		if(change_role(argv[2], permission) == 1)
+			printf("Succeed to change role\n");
+		else
+			printf("Fail to change role\n");
+		
+		return 0;
+	}
+	else if(!strcmp(argv[1], "-uc")){
+		if(argc < 4){
+			printf("Error: too few arguments\n");
+			return -1;
+		}
+		if(argc > 4){
+			printf("Error: too many arguments\n");
+			return -1;
+		}
+		
+		int uid = atoi(argv[2]);
+		if(change_user2role(uid, argv[3]) == 1)
+			printf("Succeed to change the role the of the user\n");
+		else
+			printf("Fail to change the role the of the user\n");
+		
+		return 0;
+	}
+	else if(!strcmp(argv[1], "-state")){
+		if(argc > 2){
+			printf("Error: too many arguments\n");
+			return -1;
+		}
+		
+		int state = get_state();
+		if(state == 1)
+			printf("State: Enable\n");
+		else if(state == 0)
+			printf("State: Disable\n");
+
+		return 0;
+	}
+	else if(!strcmp(argv[1], "-enable")){
+		if(argc > 2){
+			printf("Error: too many arguments\n");
+			return -1;
+		}
+
+		if(set_state(1) == 1)
+			printf("Enable!\n");
+
+		return 0;
+	}
+	else if(!strcmp(argv[1], "-disable")){
+		if(argc > 2){
+			printf("Error: too many arguments\n");
+			return -1;
+		}
+
+		if(set_state(0) == 1)
+			printf("Disable!\n");
+
+		return 0;
+	}
+	else{
+		printf("Error: invalid argument\n");
+		return -1;
+	}
+}
---
+++ security/LiangLSM/LiangLSM.c
@@ -0,0 +1,143 @@
+#include <linux/lsm_hooks.h>
+#include <linux/sysctl.h>
+#include <linux/fs.h>
+#include <linux/sched.h>
+#include <linux/cred.h>
+#include <linux/types.h>
+#include <linux/dcache.h>
+#include <linux/string.h>
+#include <linux/err.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+
+#define USER2ROLE_PATH "/etc/LiangLSM/user2role"
+#define ROLE2PERMISSION_PATH "/etc/LiangLSM/role2PERMISSION"
+#define CONTROL_PATH "/etc/LiangLSM/control"
+#define MAX_ROLENAME 20
+
+char* permission_list[] = {
+	"create file",
+	"rename file"
+};
+#define PERMISSION_COUNT (sizeof(permission_list)/sizeof(char*))
+
+int get_role(const int uid, char *role){
+	struct file *fout = filp_open(USER2ROLE_PATH, O_RDONLY, 0);
+	if(!fout || IS_ERR(fout)){
+		printk("LiangLSM: load file error\n");
+		return -1;
+	}
+
+	int _uid;
+	int res = 0;
+	while(kernel_read(fout,(char*)&uid, sizeof(int), &fout->f_pos) > 0){
+		kernel_read(fout, role, sizeof(char) *(MAX_ROLENAME+1), &fout->f_pos);
+		if(uid == _uid){
+			printk("LiangLSM: uid: %d, role: %s\n", uid, role);
+			res = 1;
+			break;
+		}
+	}
+
+	if(res == 0)
+		printk("LiangLSM: uid: %d has no role\n", uid);
+	filp_close(fout, NULL);
+	return res;
+}
+
+int role_permission(const char *role, const int op){
+	struct file *fout = filp_open(ROLE2PERMISSION_PATH, O_RDONLY, 0);
+	if(!fout || IS_ERR(fout)){
+		printk("LiangLSM: load file error\n");
+		return -1;
+	}
+
+	char _role[MAX_ROLENAME+1];
+	int _permission[PERMISSION_COUNT];
+	int res = -1;
+
+	while(kernel_read(fout, _role, sizeof(char) *(MAX_ROLENAME+1), &fout->f_pos) > 0){
+		if(strcmp(role, _role))
+			continue;
+
+		kernel_read(fout,(char*)_permission, sizeof(int) * PERMISSION_COUNT, &fout->f_pos);	
+		if(permission[_op]){
+			printk("LiangLSM: role: %s has permission %s\n", role, permission_list[_op]);
+			res = 1;
+			break;
+		}
+		else{
+			printk("LiangLSM: role: %s has no permission %s\n", role, permission_list[_op]);
+			res = 0;
+			break;
+		}
+	}
+
+	if(res == -1)
+		printk("LiangLSM: role: %s not exist", role);
+	filp_close(fout, NULL);
+	return res;
+}
+
+int is_enable(void){
+	struct file *fout = filp_open(CONTROL_PATH, O_RDONLY, 0);
+	if(!fout || IS_ERR(fout)){
+		printk("LiangLSM: load file error\n");
+		return -1;
+	}
+
+	int state;
+	kernel_read(fout,(char*)state, sizeof(int), &fout->f_pos);
+	filp_close(fout, NULL);
+
+	return state;
+}
+
+int user_permission(int uid, int op){
+	char role[MAX_ROLENAME+1];
+	if(!is_enable())
+		return 0;
+	if(uid <= 999)
+		return 0;
+	if(get_role(uid, role) != 1)
+		return 0;
+
+	if(role_permission(role, op) == 0)
+		return -1;
+	else{
+		return 0;
+	}
+}
+
+int liang_inode_create(struct inode *dir, struct dentry *dentry, umode_t mode)
+{
+	int uid = current->real_cred->uid.val;
+	printk("LiangLSM: call inode_create by uid: %d\n", uid);
+	return user_permission(uid, 0);
+}
+
+int liang_inode_rename(struct inode *old_inode, struct dentry *old_dentry, struct inode *new_inode, struct dentry *new_dentry){
+	int uid = current->real_cred->uid.val;
+	printk("LiangLSM: call inode_rename by uid: %d\n", uid);
+	return user_permission(uid, 1);
+}
+
+static struct security_hook_list liang_hooks[] = {
+    LSM_HOOK_INIT(inode_rename, liang_inode_rename),
+    LSM_HOOK_INIT(inode_create, liang_inode_create),
+};
+
+void __init liang_add_hooks(void){
+    pr_info("LiangLSM: becoming mindful.\n");
+    security_add_hooks(liang_hooks, ARRAY_SIZE(liang_hooks), "liang");
+}
+
+static __init int liang_init(void){
+    liang_add_hooks();
+    return 0;
+}
+
+DEFINE_LSM(liang) = {
+	.name = "liang",
+	.init = liang_init,
+};
